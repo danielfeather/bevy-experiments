ffc08c82fcb9983f50804880b688d1fc43771146
Daniel Feather <12731372+danielfeather@users.noreply.github.com>
2024-09-04 10:56:48 +0100

    Changed to hash based history router and fixed iframe src

--- www/src/views/GameView.vue
+++ www/src/views/GameView.vue
@@ -2,18 +2,22 @@
   <div class="container">
     <RouterLink to="/">Back</RouterLink>
     <h1>{{ $route.params.experiment }}</h1>
-    <iframe class="bevy-game" :src="`/bevy/${$route.params.experiment}`" frameborder="0"></iframe>
+    <iframe
+      class="bevy-game"
+      :src="`${import.meta.env.BASE_URL}/bevy/${$route.params.experiment}`"
+      frameborder="0"
+    ></iframe>
   </div>
 </template>
 
 <style>
-  .container {
-    margin: 0 auto;
-    max-width: 1200px;
-  }
-  .bevy-game {
-    width: 100%;
-    height: 100%;
-    aspect-ratio: 1.7777;
-  }
+.container {
+  margin: 0 auto;
+  max-width: 1200px;
+}
+.bevy-game {
+  width: 100%;
+  height: 100%;
+  aspect-ratio: 1.7777;
+}
 </style>
